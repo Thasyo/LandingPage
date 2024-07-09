34956c0caea729050f792928ea969f7ff91a1e62
Thasyo Peres <thasyotp2002@gmail.com>
2024-07-08 22:36:26 -0300

    add responsive header

---
+++ assets/js/responsive.js
@@ -0,0 +1,5 @@
+/* responsive header */
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    let menu = document.querySelector('.container-itens');
+    menu.classList.toggle('active');
+});
